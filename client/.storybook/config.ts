e19075752279a0a5b02af90e2fd629db53425bf3
Brandon Ly <6823589+brandonly101@users.noreply.github.com>
2019-09-04 00:13:08 -0700

    Enable use of TypeScript in project

--- client/.storybook/config.ts
+++ client/.storybook/config.ts
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+// automatically import all files ending in *.stories.js/jsx/ts/tsx
+const req = require.context('../stories', true, /\.stories\.(js|jsx|ts|tsx)$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
