2df1ae712b67ca26a2b1be5f15e285da7177cfd8
Cat McLoughlin <cmcloughlin@8thlight.com>
2019-09-03 20:28:20 -0700

    Separate Form into its own storybook file
    Remove unnecessary button example

---
+++ client/stories/Forms.stories.js
@@ -0,0 +1,160 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { Formik, Form, Field } from 'formik';
+import {
+  BaseDatePicker,
+  BaseTimePicker,
+  BaseRadio,
+  BaseSelect,
+  BaseCheckbox,
+  BaseSlider,
+} from '../src/components/Forms/Base';
+import ExampleForm from '../src/components/Forms/FormikForms/ExampleForm';
+
+storiesOf('Forms', module)
+  .add('Input', () => (
+    <ExampleForm
+      initValues={{ Firstname: '', LastName: '', FavoriteColor: '' }}
+    />
+  ))
+  .add('Radio', () => (
+    <Formik
+      initialValues={{ name: 'Tony' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={() => (
+        <Form>
+          <Field
+            id="MyRadio"
+            label="MyRadioLabel"
+            name="name"
+            component={BaseRadio}
+          />
+        </Form>
+      )}
+    />
+  ))
+  .add('Select', () => (
+    <Formik
+      initialValues={{ color: 'red' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={() => (
+        <Form>
+          <Field
+            name="color"
+            label="Color"
+            valueOptions={[
+              { value: 'red', label: 'Red' },
+              { value: 'green', label: 'Green' },
+              { value: 'blue', label: 'Blue' },
+            ]}
+            component={BaseSelect}
+          />
+        </Form>
+      )}
+    />
+  ))
+  .add('Checkbox', () => (
+    <Formik
+      initialValues={{ color: 'red' }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={() => (
+        <Form>
+          <Field
+            name="color"
+            label="Color"
+            valueOptions={[
+              { value: 'red', label: 'Red' },
+              { value: 'green', label: 'Green' },
+              { value: 'blue', label: 'Blue' },
+            ]}
+            component={BaseCheckbox}
+          />
+        </Form>
+      )}
+    />
+  ))
+  .add('TimePicker', () => (
+    <Formik
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={({
+        handleSubmit,
+        isSubmitting,
+        values,
+        handleReset,
+        ...props
+      }) => (
+        <Form>
+          <Field
+            name="TimePicker"
+            label="TimePicker"
+            component={BaseTimePicker}
+            {...props}
+          />
+        </Form>
+      )}
+    />
+  ))
+  .add('DatePicker', () => (
+    <Formik
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={({
+        handleSubmit,
+        isSubmitting,
+        values,
+        handleReset,
+        ...props
+      }) => (
+        <Form>
+          <Field
+            name="DatePicker"
+            label="DatePicker"
+            component={BaseDatePicker}
+            {...props}
+          />
+        </Form>
+      )}
+    />
+  ))
+  .add('Slider', () => (
+    <Formik
+      initialValues={{ happiness: 5 }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={() => (
+        <Form>
+          <Field
+            name="happiness"
+            label="Happiness Level"
+            min={0}
+            max={5}
+            step={1}
+            component={BaseSlider}
+          />
+        </Form>
+      )}
+    />
+  ));
