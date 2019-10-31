4b9ccb049143161bac2c943fb2995789401e3a7e
Kenneth Lee <kennyxlee@gmail.com>
2019-10-30 18:25:35 -0700

    Remove Layout component from HomePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import { SectionContainer } from '../components/common';
-import Layout from './Layout';
 
 export const HomePage = () => {
   return (
-    <Layout>
-      <SectionContainer>
-        <Typography variant="h3">Welcome!</Typography>
-        <Typography variant="h5">Shared housing</Typography>
-      </SectionContainer>
-    </Layout>
+    <SectionContainer>
+      <Typography variant="h3">Welcome!</Typography>
+      <Typography variant="h5">Shared housing</Typography>
+    </SectionContainer>
   );
 };
 export default HomePage;
