ea8b6e17f29a47f9b022c10d7deaa27668ea31bb
Joshua Ramirez <josh20615@gmail.com>
2019-11-13 21:52:41 -0800

    update REST paths and change import to sqlalchemy direcorty

--- server/api/routes/sqlalchemy/candidates_response.py
+++ server/api/routes/sqlalchemy/candidates_response.py
@@ -3,17 +3,16 @@ import os
 import sys
 sys.path.append(os.path.realpath('.'))
 from flask import Blueprint, request
-from models.models import QuestionResponse, ResponseSchema, db
+from models.sqlalchemy.models import QuestionResponse, ResponseSchema, db
 
 response_schema = ResponseSchema()
 response_routes = Blueprint("response_routes", __name__)
 
 # Below we have "candidateid" hardcoded, this should change in the future
-@response_routes.route('candidateId/response', methods=['POST'])
-def add_response():
+@response_routes.route('/<candidateId>/response', methods=['POST'])
+def add_response(candidateId):
     responseValue = request.json['responseValue']
-    candidateId = request.json['candidateId']
- 
+
     new_response = QuestionResponse(responseValue, candidateId)
  
     db.session.add(new_response)
@@ -22,8 +21,8 @@ def add_response():
     return response_schema.jsonify(new_response)
 
 @response_routes.route('/<questionId>/responses', methods=['GET'])
-def get_response(questionid):
-    product = QuestionResponse.query.get(id)
+def get_response(questionId):
+    product = QuestionResponse.query.get(questionId)
     return response_schema.jsonify(product)
 
 # Below I hardcoded candidateid and responseid
