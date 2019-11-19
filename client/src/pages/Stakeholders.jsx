299685995bac868cc20679dc6aa5462aa8088003
William Buck <wsbuckiii@gmail.com>
2019-11-18 23:33:53 -0800

    Update Stakeholders.jsx

--- client/src/pages/Stakeholders.jsx
+++ client/src/pages/Stakeholders.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import Modal from '@material-ui/core/Modal';
 import Grid from '@material-ui/core/Grid';
 import LAFHFlyer from '../assets/images/Stakeholders/LAFH_flyer.png';
 import { SectionContainer } from '../components/common';
@@ -10,12 +10,12 @@ import { SectionContainer } from '../components/common';
 const useStyles = makeStyles(theme => ({
   stakeholdersTitle: {
     color: 'gray',
-    margin: '20px 0 20px 0',
     textDecoration: 'underline',
-    textAlign: 'center',
   },
-  gridPadding: {
-    padding: '50px',
+  gridContainer: {
+    // padding: '50px',
+    width: 1000,
+    maxWidth: '100%',
   },
   stakeholdersListTitle: {
     textDecoration: 'underline',
@@ -24,14 +24,31 @@ const useStyles = makeStyles(theme => ({
     margin: '10px 0',
   },
   flyerModal: {
-    position: 'absolute',
+    // position: 'absolute',
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    // top: '50%',
+    // left: '50%',
+    // transform: 'translate(-50%, -50%)',
+  },
+  flyerContainer: {
+    maxWidth: '100%',
+  },
+  flyerPreview: {
+    width: '100%',
+  },
+  flyerImage: {
+    height: 'auto',
+    maxWidth: '100%',
+  },
+  container: {
+    padding: theme.spacing(2),
+    width: 800,
+    maxWidth: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
 }));
 
@@ -45,62 +62,56 @@ const STAKEHOLDERS = [
 
 export const StakeholdersPage = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
     <SectionContainer>
-      <Typography className={classes.stakeholdersTitle} variant="h5">
-        Sponsor LA Family Housing
-      </Typography>
-
-      <Grid container spacing={3} className={classes.gridPadding}>
-        <Grid item xs={6}>
-          <List>
-            <Typography variant="h6" className={classes.stakeholdersListTitle}>
-              Stakeholders
+      <Paper className={classes.container}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography className={classes.stakeholdersTitle} variant="h4">
+              Sponsor
             </Typography>
+            <div className={classes.holder}>
+              <Typography variant="body1">LA Family Housing</Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <List>
+              <Typography
+                variant="h5"
+                className={classes.stakeholdersListTitle}
+              >
+                Stakeholders
+              </Typography>
 
-            {STAKEHOLDERS.map(holder => {
-              return (
-                <div key={holder} className={classes.holder}>
-                  {holder}
-                </div>
-              );
-            })}
-          </List>
+              {STAKEHOLDERS.map(holder => {
+                return (
+                  <li key={holder} className={classes.holder}>
+                    {holder}
+                  </li>
+                );
+              })}
+            </List>
+          </Grid>
+
+          <Grid item xs={12} md={3}>
+            <a
+              className={classes.flyerContainer}
+              href={LAFHFlyer}
+              tabIndex={0}
+              type="button"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={LAFHFlyer}
+                alt="lafhFlyer"
+                className={classes.flyerPreview}
+              />
+            </a>
+          </Grid>
         </Grid>
-
-        <Grid item xs={6}>
-          <div
-            className="flyer-container"
-            onClick={handleOpen}
-            role="button"
-            tabIndex={0}
-            onKeyPress={handleOpen}
-          >
-            <img src={LAFHFlyer} alt="lafhFlyer" width="500" hegith="500" />
-          </div>
-        </Grid>
-      </Grid>
-
-      <Modal
-        aria-labelledby="flyer-modal-title"
-        aria-describedby="flyer-modal-description"
-        open={open}
-        onClose={handleClose}
-      >
-        <div className={classes.flyerModal}>
-          <img src={LAFHFlyer} alt="lafhFlyer" width="800" height="900" />
-        </div>
-      </Modal>
+      </Paper>
     </SectionContainer>
   );
 };
