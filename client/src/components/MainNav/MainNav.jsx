f0d37eb00168437826e61ef81b0e33d9529207c1
William Buck <wsbuckiii@gmail.com>
2019-11-22 18:03:32 -0800

    Added navbar hamburger functionality
    * Refactored navbar buttons rendering
    * Added Menu and MenuItems for hamburger button
    * Only show Hamburger when screen size is small

--- client/src/components/MainNav/MainNav.jsx
+++ client/src/components/MainNav/MainNav.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { withRouter } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,9 +23,6 @@ const useStyles = makeStyles(theme => ({
   ToolBar: {
     display: 'flex',
     justifyContent: 'space-between',
-  },
-  LeftContent: {
-    width: '100%',
   },
   RightContent: {
     width: '100%',
@@ -32,87 +36,141 @@ const useStyles = makeStyles(theme => ({
   button: {
     marginRight: theme.spacing(2),
     color: 'white',
+    display: 'block',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
   MenuIcon: {
     verticalAlign: 'middle',
     marginRight: theme.spacing(2),
   },
+  title: {
+    flexGrow: 0.5,
+    marginLeft: theme.spacing(1),
+    textAlign: 'left',
+  },
+  paper: {
+    background: '#3f51b5',
+    color: 'white',
+  },
+  menuButton: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  navMenuItem: {
+    color: 'inherit',
+  },
 }));
 
-const MainNav = () => {
+function MainNav(props) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
 
-  const buttonWithRoute = ({ name, onClick, path }) => {
-    const handleClick = history => () => {
-      if (onClick) {
-        onClick();
-      }
-      history.push(path);
-    };
+  function handleMenuButtonClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
 
-    return withRouter(({ history }) => (
-      <Button classes={{ root: classes.button }} onClick={handleClick(history)}>
-        {name}
-      </Button>
-    ));
+  function handleMenuButtonClose() {
+    setAnchorEl(null);
+  }
+
+  const paths = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'Dashboard',
+      path: '/dashboard',
+    },
+    {
+      name: 'News',
+      path: '/news',
+    },
+    {
+      name: 'Demo',
+      path: '/demo',
+    },
+    {
+      name: 'Prototype-Form',
+      path: '/prototype-form',
+    },
+    {
+      name: 'Stakeholders',
+      path: '/stakeholders',
+    },
+    {
+      name: 'Team Roster',
+      path: '/team',
+    },
+    {
+      name: 'Agile Manifesto',
+      path: '/agile-manifesto',
+    },
+  ];
+
+  const MenuButton = () => {
+    function handleClick(path) {
+      handleMenuButtonClose();
+      props.history.push(path);
+    }
+
+    return (
+      <div>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleMenuButtonClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="navMenu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuButtonClose}
+          classes={{ paper: classes.paper }}
+        >
+          {paths.map(path => (
+            <MenuItem key={path.name} onClick={() => handleClick(path.path)}>
+              {path.name}
+            </MenuItem>
+          ))}
+        </Menu>
+      </div>
+    );
   };
-
-  const HomeButton = buttonWithRoute({
-    name: 'Home',
-    path: '/',
-  });
-
-  const DashboardButton = buttonWithRoute({
-    name: 'Dashboard',
-    path: '/dashboard',
-  });
-
-  const NewsButton = buttonWithRoute({
-    name: 'News',
-    path: '/news',
-  });
-
-  const DemoButton = buttonWithRoute({
-    name: 'Demo',
-    path: '/demo',
-  });
-
-  const PrototypeFormButton = buttonWithRoute({
-    name: 'Prototype-Form',
-    path: '/prototype-form',
-  });
-  const StakeholdersButton = buttonWithRoute({
-    name: 'Stakeholders',
-    path: '/stakeholders',
-  });
-  const TeamRosterButton = buttonWithRoute({
-    name: 'Team Roster',
-    path: '/team',
-  });
-  const AgileManifestoButton = buttonWithRoute({
-    name: 'Agile Manifesto',
-    path: '/agile-manifesto',
-  });
 
   return (
     <nav className={classes.root}>
       <AppBar color="primary">
         <Toolbar className={classes.ToolBar}>
-          <div className={classes.LeftContent}>
-            <MenuIcon className={classes.MenuIcon} />
-            <HomeButton />
-            <NewsButton />
-            <DashboardButton />
-            <DemoButton />
-            <PrototypeFormButton />
-            <StakeholdersButton />
-            <TeamRosterButton />
-            <AgileManifestoButton />
-          </div>
+          <MenuButton className={classes.menuButton} />
+          <Typography variant="h6" className={classes.title} noWrap>
+            Shared Housing
+          </Typography>
+          {paths.map(path => (
+            <Button
+              key={path.name}
+              classes={{ root: classes.button }}
+              onClick={() => props.history.push(path.path)}
+            >
+              {path.name}
+            </Button>
+          ))}
         </Toolbar>
       </AppBar>
     </nav>
   );
+}
+
+MainNav.propTypes = {
+  history: PropTypes.instanceOf(Object).isRequired,
 };
 
-export default MainNav;
+export default withRouter(MainNav);
