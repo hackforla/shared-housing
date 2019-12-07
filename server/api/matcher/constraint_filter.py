7c13150f57c75b265e21d39b110a734263ce2673
Joshua Ramirez <josh20615@gmail.com>
2019-12-06 23:48:55 -0800

    write function which adds and removes location/candidate pairings based on eligibility

--- server/api/matcher/constraint_filter.py
+++ server/api/matcher/constraint_filter.py
@@ -1,4 +1,6 @@
-from models.sqlalchemy.models import Candidate, Location, QuestionResponse, LocationResponse
+from models.sqlalchemy.models import Candidate, QuestionResponse, LocationResponse, CandidateLocation, db
+
+from utils_ivan import eligibility
 
 
 def recalculate(location_id: int):
@@ -8,10 +10,27 @@ def recalculate(location_id: int):
             location_id(int): location identifier used to gather relevant comparison data
     """
 
-    # location = Location.query.get(location_id).first()
-    candidates = Candidate.query.get.all()
+    # TODO(JOSH): Gets all candidates for filtering. Seems like there is room for optimization here. Potentially only
+    #  pull back candidates who have been added or modified since after last recalculating
+    candidates = Candidate.query.all()
 
-    location_responses = LocationResponse.query.filter_by(locationId=location_id).all()
+    location_responses = LocationResponse.query.filter_by(locationId=location_id) \
+        .order_by(LocationResponse.questionId).all()
     for candidate in candidates:
-        candidate_responses = QuestionResponse.query.filter_by(candidateId=candidate['candidateId']).all()
-        eligible = True
+        candidate_responses = QuestionResponse.query.filter_by(candidateId=candidate.candidateId) \
+            .order_by(QuestionResponse.questionId).all()
+
+        # Check to see if the candidate and location do not violate any hard constraints
+        if eligibility.candidate_eligible_for_location(candidate_responses, location_responses):
+            # Check to see if the candidate location combination has already been added
+            candidate_location = CandidateLocation.query.get((candidate.candidateId, location_id))
+            if candidate_location is None:
+                eligible_candidate = CandidateLocation(candidate.candidateId, location_id)
+
+                db.session.add(eligible_candidate)
+                db.session.commit()
+        else:
+            candidate_location = CandidateLocation.query.get((candidate.candidateId, location_id))
+            if candidate_location is not None:
+                CandidateLocation.query.filter_by(candidateId=candidate.candidateId, locationId=location_id)\
+                    .delete(synchronize_session=False)
