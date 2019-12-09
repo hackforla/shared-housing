6d2f65c2a5d3f89d3401facdbc884c0b6a700ad4
itsevalieu <itsevalieu@gmail.com>
2019-12-08 22:16:35 -0800

    Move previous demo to PreviousDemo component, created Results and TenantForm

---
+++ client/src/components/Demo/Results.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Results = () => {
+    return (
+        <div>
+            <h2>Results</h2>
+            <ul>
+                <li>Tenant 1</li>
+                <li>Tenant 2</li>
+                <li>Tenant 3</li>
+            </ul>
+        </div>
+    );
+};
