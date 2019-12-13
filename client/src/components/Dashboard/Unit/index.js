6d330fcfd53ddeee4a0bccebc9a42a1086d413d2
William Buck <wsbuckiii@gmail.com>
2019-12-13 15:44:02 -0800

    Add UnitCard React Component
    created a UnitCard react component for use in Unit dashboard. Takes in a variety of props that are then formatted in the card wireframe outlined.
    
    * Added to storybook
    * Used material-ui
    * Takes in props
    * Typescript
    * resolves #262

---
+++ client/src/components/Dashboard/Unit/index.js
@@ -0,0 +1,1 @@
+export { UnitCard } from './UnitCard';
--- client/stories/Dashboard.stories.js
+++ client/stories/Dashboard.stories.js
@@ -6,7 +6,19 @@ import {
   BaseSidebar,
 } from '../src/components/Dashboard/Base';
 
+import { UnitCard } from '../src/components/Dashboard/Unit';
+
 storiesOf('Dashboard', module)
   .add('BaseAvatar', () => <BaseAvatar />)
   .add('BaseChip', () => <BaseChip label="Smoker" color="primary" />)
-  .add('BaseSidebar', () => <BaseSidebar />);
+  .add('BaseSidebar', () => <BaseSidebar />)
+  .add('UnitCard', () => (
+    <UnitCard
+      name="Unit 1"
+      image="http://lorempixel.com/300/300/"
+      address1="123 Happy Lane"
+      address2="Los Angeles, CA 90026"
+      handicapAccessible
+      matches={32}
+    />
+  ));
