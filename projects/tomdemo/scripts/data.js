2b16d43291011ba888c844a34c637340fdb4650e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-12-28 03:13:13 -0800

    Refactor Tom Demo from HTML to JS

--- projects/tomdemo/scripts/data.js
+++ projects/tomdemo/scripts/data.js
@@ -1,26 +1,85 @@
-data = {
-    "tags": [
-        "Bird",
-        "Cat",
-        "Dog",
-        "Red",
-        "Green",
-        "Blue"
+////
+//
+// JSON data
+//
+////
+
+let data = {
+    "makerTags": [
+        {
+            "type": "tag",
+            "attributes": {
+                "name": "Bird",
+            }
+        },{
+            "type": "tag",
+            "attributes": {
+                "name": "Cat",
+            }
+        },{
+            "type": "tag",
+            "attributes": {
+                "name": "Dog",
+            }
+        }
+    ],
+    "takerTags": [
+        {
+            "type": "tag",
+            "attributes": {
+                "name": "Red",
+            }
+        },{
+            "type": "tag",
+            "attributes": {
+                "name": "Green",
+            }
+        },{
+            "type": "tag",
+            "attributes": {
+                "name": "Blue",
+            }
+        }
     ],
     "makers": [
-        "Apartment A",
-        "Apartment B",
-        "Apartment C",
-        "Apartment D",
-        "Apartment E",
-        "Apartment F"
+        {
+            "type": "maker",
+            "attributes": {
+                "freeform0": "Apartment A"
+            }
+        }
     ],
     "takers": [
-        "Alice",
-        "Bob",
-        "Carol",
-        "Dave",
-        "Eve",
-        "Frank",
-    ],
+        {
+            "type": "taker",
+            "attributes": {
+                "freeform0": "Alice",
+            }
+        },{
+            "type": "taker",
+            "attributes": {
+                "freeform0": "Bob",
+            }
+        },{
+            "type": "taker",
+            "attributes": {
+                "freeform0": "Carol",
+            }
+        },{
+            "type": "taker",
+            "attributes": {
+                "freeform0": "David",
+            }
+        },{
+            "type": "taker",
+            "attributes": {
+                "freeform0": "Eve",
+            }
+        },{
+            "type": "taker",
+            "attributes": {
+                "freeform0": "Frank",
+            }
+        }
+    ]
 }
