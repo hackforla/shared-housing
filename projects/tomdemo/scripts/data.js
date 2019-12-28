540b1a4c5a3fc6b78932bdec6e6123f2255ccfc3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-12-27 23:54:10 -0800

    Add Tom Demo scripts JavaScript file

---
+++ projects/tomdemo/scripts/data.js
@@ -0,0 +1,26 @@
+data = {
+    "tags": [
+        "Bird",
+        "Cat",
+        "Dog",
+        "Red",
+        "Green",
+        "Blue"
+    ],
+    "makers": [
+        "Apartment A",
+        "Apartment B",
+        "Apartment C",
+        "Apartment D",
+        "Apartment E",
+        "Apartment F"
+    ],
+    "takers": [
+        "Alice",
+        "Bob",
+        "Carol",
+        "Dave",
+        "Eve",
+        "Frank",
+    ],
+}
